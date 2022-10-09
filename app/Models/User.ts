795c0d1570fba7c307084474093df0c1a2f3150c
Icaro Vieira <icarovsilva1@gmail.com>
2022-10-09 20:36:13 -0300

    feat: create User controller

---
+++ app/Models/User.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, beforeCreate } from '@ioc:Adonis/Lucid/Orm'
+import { randomUUID } from 'node:crypto'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public username: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword(User: User) {
+    if (User.$dirty.password) {
+      User.password = await Hash.make(User.password)
+    }
+  }
+
+  @beforeCreate()
+  public static attatchId(user: User) {
+    user.id = randomUUID()
+  }
+}
