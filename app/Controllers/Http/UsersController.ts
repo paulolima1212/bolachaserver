fddafad23454c81af28094f3bd6995e50b119360
Icaro Vieira <icarovsilva1@gmail.com>
2022-10-09 21:08:45 -0300

    feat(user): create user rotues and adding hash from password

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,3 +1,29 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import CreateUserValidator from 'App/Validators/CreateUserValidator'
 
-export default class UsersController {}
+export default class UsersController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const { email, password, username } = await request.validate(CreateUserValidator)
+
+    try {
+      await User.create({
+        email,
+        password,
+        username,
+      })
+
+      const token = await auth.use('api').attempt(email, password, {
+        expiresIn: '2hours',
+      })
+
+      return response.created({
+        user: auth.user,
+        token,
+      })
+    } catch (err) {
+      console.log(err)
+      return response.internalServerError()
+    }
+  }
+}
